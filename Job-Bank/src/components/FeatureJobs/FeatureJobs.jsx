e481dd72eb5006609d41a4d4072d4c5ee8105cd3
FAHAD KABIR <hmdipto7@gmail.com>
2024-08-14 13:57:00 -0230

    Job, featurejob layout & functionality added

--- Job-Bank/src/components/FeatureJobs/FeatureJobs.jsx
+++ Job-Bank/src/components/FeatureJobs/FeatureJobs.jsx
@@ -1,31 +1,39 @@
-
 import { useEffect } from "react";
 import Job from "../Job/job";
 import { useState } from "react";
 
 const FeatureJobs = () => {
   const [Jobs, setJobs] = useState([]);
+  const [datalength, setDataLength] = useState(4);
   useEffect(() => {
     fetch("jobs.json")
       .then((res) => res.json())
       .then((data) => setJobs(data));
   }, []);
 
-  console.log(Jobs)
+  console.log(Jobs);
   return (
     <div>
       <div className=" text-center ">
         <h2 className="text-5xl font-bold">Feature Jobs {Jobs.length}</h2>
         <p className="text-3xl">
-         
           Explore thousands of job opportunities with all the information you
           need. Its your future
         </p>
       </div>
-      <div className="">
-        {Jobs.map((job) => (
+      <div className=" grid grid-cols-2 gap-4">
+        {Jobs.slice(0, datalength).map((job) => (
           <Job key={job.id} job={job}></Job>
         ))}
+      </div>
+      <div  
+      className= { datalength ===Jobs.length && 'hidden'}>
+        <button
+          onClick={() => setDataLength(Jobs.length)}
+          className="btn btn-outline btn-primary px-10 "
+        >
+          View All
+        </button>
       </div>
     </div>
   );
--- Job-Bank/src/components/Job/Job.jsx
+++ Job-Bank/src/components/Job/Job.jsx
@@ -1,13 +1,43 @@
+import { IoLocationSharp } from "react-icons/io5";
+import { MdAttachMoney } from "react-icons/md";
 
-
-const Job = ({job}) => {
-    const {logo} = job
-    return (
-        <div>
-            <img src={logo} alt="" />
-            
+const Job = ({ job }) => {
+  const {
+    logo,
+    id,
+    job_title,
+    company_name,
+    remote_or_onsite,
+    location,
+    job_type,
+    salary,
+  } = job;
+  return (
+    <div className="card card-compact bg-base-100  shadow-xl">
+      <figure>
+        <img src={logo} alt="Shoes" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{job_title}</h2>
+        <p>{company_name}</p>
+        <div className="">
+          <button className="btn btn-outline btn-info mr-3">
+            {remote_or_onsite}
+          </button>
+          <button className="btn btn-outline btn-info">{job_type}</button>
         </div>
-    );
+        <div className="mt-4 flex gap-2">
+          <h2 className=" flex">
+            <IoLocationSharp className="text-2xl mr-1" /> {location}
+          </h2>
+          <h2 className=" flex "><MdAttachMoney className="text-xl  " />{salary}</h2>
+        </div>
+        <div className="card-actions ">
+          <button className="btn btn-success">View Details</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Job;
